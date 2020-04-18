a6d226c4da13f9b39d7066b0d981010080c1815b
Usman Iqbal <usman.iqbal@softwire.com>
2020-04-18 15:25:28 +0100

    Add a logger for messages

--- src/config/middleware.php
+++ src/config/middleware.php
@@ -1,7 +1,9 @@
 <?php
 
+use Psr\Log\LogLevel;
 use Selective\Config\Configuration;
 use SimpleContainer\ContainerBuilder;
+use SimpleLog\Logger;
 use Slim\App;
 
 return function (App $app) {
@@ -13,11 +15,21 @@ return function (App $app) {
 
     // Add error handler middleware
     $container = ContainerBuilder::getContainer();
-    $settings = ($container->get('Settings'))->get('error_handler_middleware');
+    $settings = $container->get('Settings');
 
-    $displayErrorDetails = (bool)$settings['display_error_details'];
-    $logErrors = (bool)$settings['log_errors'];
-    $logErrorDetails = (bool)$settings['log_error_details'];
 
-    $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
+    $slim_error_handler_settings = $settings->get('error_handler_middleware');
+    $display_error_details = (bool)$slim_error_handler_settings['display_error_details'];
+    $log_errors = (bool)$slim_error_handler_settings['log_errors'];
+    $log_error_details = (bool)$slim_error_handler_settings['log_error_details'];
+
+    $logger_settings = $settings->get('logger');
+    $log_level = ((bool)$logger_settings['debug']) ? LogLevel::DEBUG : LogLevel::INFO; 
+    $logger  = new Logger($logger_settings['path'], $logger_settings['name'], $log_level);
+
+    if ((bool)$logger_settings['include_stdout']) {
+        $logger->setOutput(true);
+    }
+
+    $app->addErrorMiddleware($display_error_details, $log_errors, $log_error_details, $logger);
 };
